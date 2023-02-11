6f3cc563013563e4ba4060f926d88803ec05e694
Gary Hetzel <its@gary.cool>
2023-02-10 23:53:35 -0500

    adding gortboard

--- keyboards/keebsforall/freebirdnp/pro/keymaps/gortpad/keymap.c
+++ keyboards/keebsforall/freebirdnp/pro/keymaps/gortpad/keymap.c
@@ -16,21 +16,29 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = LAYOUT_numpad_6x4(
-    MO(1),   KC_VOLD, KC_VOLU,  KC_KB_MUTE,
-    MO(1),   KC_PSLS, KC_PAST, KC_PMNS,
-    KC_P7,   KC_P8,   KC_P9,
-    KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-    KC_P1,   KC_P2,   KC_P3,
-    KC_P0,            KC_PDOT, KC_PENT ),
+    [0] = LAYOUT_numpad_6x4(
+        MO(1),   KC_VOLD, KC_VOLU,  KC_KB_MUTE,
+        KC_NO,   KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
+        KC_P1,   KC_P2,   KC_P3,
+        KC_P0,            KC_PDOT, KC_PENT ),
 
-[1] = LAYOUT_numpad_6x4(
-    KC_TRNS, KC_MRWD, KC_MFFD, KC_F16,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,          KC_TRNS, QK_BOOT)
+    [1] = LAYOUT_numpad_6x4(
+        KC_TRNS, KC_MRWD, KC_MFFD, KC_F16,
+        TG(2),   KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_TRNS,
+        KC_TRNS,          KC_TRNS, QK_BOOT),
+
+    [2] = LAYOUT_numpad_6x4(
+        TO(0),   KC_MRWD, KC_MFFD, KC_F16,
+        TO(0),   KC_TRNS, KC_TRNS, KC_WH_U,
+        KC_WH_L, KC_MS_U, KC_WH_R,
+        KC_MS_L, KC_BTN1, KC_MS_R, KC_WH_D,
+        KC_WH_L, KC_MS_D, KC_WH_R,
+        KC_BTN1,          KC_BTN2, KC_BTN3)
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
--- keyboards/keebsforall/freebirdtkl/config.h
+++ keyboards/keebsforall/freebirdtkl/config.h
@@ -42,3 +42,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define LOCKING_SUPPORT_ENABLE
 #define LOCKING_RESYNC_ENABLE
+
+#define DYNAMIC_MACRO_NO_NESTING
